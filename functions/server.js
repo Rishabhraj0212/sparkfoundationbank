6840072e45b62c40d84f86e5f7b97386735cc323
Rishabh <rishabhraj021official@gmail.com>
2025-03-29 16:20:25 +0530

    Update Netlify configuration and serverless function

--- functions/server.js
+++ functions/server.js
@@ -1,4 +1,23 @@
 const serverless = require('serverless-http');
-const app = require('../index.js');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
 
-module.exports.handler = serverless(app); 
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '..')));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '../views'));
+
+// Routes
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+// Import your routes
+const router = require('../router/router');
+app.use('/', router);
+
+// Export the serverless handler
+exports.handler = serverless(app); 
